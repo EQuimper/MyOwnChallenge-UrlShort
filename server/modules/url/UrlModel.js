36815d72b5a49c7fa3f00deb0c45ecbedd699a6f
EQuimper <quimperemanuel@gmail.com>
2016-12-11 18:20:58 -0500

    Make a unique random base64 url

--- server/modules/url/UrlModel.js
+++ server/modules/url/UrlModel.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+import crypto from 'crypto';
 
 const UrlSchema = new Schema({
   longUrl: { type: String, unique: true, required: true },
@@ -9,16 +10,28 @@ const UrlSchema = new Schema({
   }
 });
 
-const makeUniqueUrl = url => {
-
-}
-
-UrlSchema.pre('save', function(next) {
-  makeUniqueUrl(this.longUrl);
-  this.shortUrl = '123457';
-  next();
-});
+const makeUniqueUrl = len => {
+  return crypto
+    .randomBytes(Math.ceil(len * (3 / 4)))
+    .toString('base64')
+    .slice(0, len)
+    .replace(/\+/g, '0')
+    .replace(/\//g, '0');
+};
 
 const Url = mongoose.model('Url', UrlSchema);
+
+UrlSchema.pre('save', true, function(next, done) {
+  this.shortUrl = makeUniqueUrl(6);
+  mongoose.models.Url.findOne({ shortUrl: this.shortUrl })
+    .then(url => {
+      if (url) {
+        this.shortUrl = makeUniqueUrl(6);
+      }
+      return done();
+    })
+    .catch(err => done(err));
+  next();
+});
 
 export default Url;
