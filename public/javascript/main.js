da1770288c266d695d1706a1017f65435e3d9952
EQuimper <quimperemanuel@gmail.com>
2016-12-11 21:52:59 -0500

    Add validation client side and copyboard

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -11,30 +11,56 @@ const shorten = function() {
     dataType: 'JSON',
     data: { longUrl: $('#url-to-short').val() },
     success: function(data) {
-      $('input[name=url]').val(`shneed.com/${data.url.shortUrl}`);
+      $('input[name=url]').parent().addClass('has-success');
+      successClick(data);
+      resetAppear();
+    },
+    error: function(err) {
+      $('input[name=url]').parent().addClass('has-error');
+      $('input[name=url]').val(err.responseJSON.message);
       $('input[name=url]').prop('readOnly', true);
-      $('#btn-short').unbind('click.shorten');
-      $('#btn-short').html('<span class="glyphicon glyphicon-copy" aria-hidden="true"></span>');
-      $('#btn-short').prop('data-toggle', 'tooltip');
-      $('#btn-short').prop('data-placement', 'right');
-      $('#btn-short').prop('title', 'Copy to clipboard');
-      $('#btn-short').tooltip();
-
-      const resetHTML = '<button class="btn btn-danger btn-lg" id="reset-btn">Reset</button>';
-
-      $('#reset-link').html(resetHTML)
-      $('#reset-link').hide().fadeIn('slow');
-
-      $('#reset-btn').click(function() {
-        $('#reset-link').empty();
-        $('#btn-short').bind('click.shorten', shorten);
-        $('input[name=url]').val('');
-        $('input[name=url]').prop('readOnly', false);
-        $('#btn-short').html('Short Need');
-        $('#btn-short').prop('data-toggle', false);
-        $('#btn-short').prop('data-placement', false);
-        $('#btn-short').prop('title', false);
-      });
+      $('#btn-short').prop('disabled', true);
+      $('#btn-short').html('Error Happen');
+      $('#btn-short').addClass('btn-danger');
+      resetAppear();
     }
   });
 };
+
+const successClick = function(data) {
+  $('input[name=url]').val(`shneed.com/${data.url.shortUrl}`);
+  $('input[name=url]').prop('readOnly', true);
+  $('#btn-short').unbind('click.shorten');
+  new Clipboard('#btn-short');
+  $('#btn-short').attr('data-clipboard-target', '#url-to-short');
+  $('#btn-short').html('<span class="glyphicon glyphicon-copy" aria-hidden="true"></span>');
+  $('#btn-short').prop('data-toggle', 'tooltip');
+  $('#btn-short').addClass('btn-success');
+  $('#btn-short').prop('data-placement', 'right');
+  $('#btn-short').prop('title', 'Copy to clipboard');
+  $('#btn-short').tooltip();
+}
+
+const resetClick = function() {
+  $('#reset-link').empty();
+  $('#btn-short').bind('click.shorten', shorten);
+  $('input[name=url]').val('');
+  $('input[name=url]').prop('readOnly', null);
+  $('#btn-short').attr('data-clipboard-target', null);
+  $('#btn-short').html('Short Need');
+  $('#btn-short').prop('data-toggle', null);
+  $('#btn-short').prop('data-placement', null);
+  $('#btn-short').prop('title', null);
+  $('#btn-short').prop('disabled', null);
+  $('#btn-short').removeClass('btn-success');
+  $('#btn-short').removeClass('btn-danger');
+  $('input[name=url]').parent().removeClass('has-error');
+  $('input[name=url]').parent().removeClass('has-success');
+};
+
+const resetAppear = function() {
+  const resetHTML = '<button class="btn btn-danger btn-lg" id="reset-btn">Reset</button>';
+  $('#reset-link').html(resetHTML)
+  $('#reset-link').hide().fadeIn('slow');
+  $('#reset-btn').click(resetClick);
+}
