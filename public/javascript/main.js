c9aa81146b4eeb7b21e1d0eb22d3e29766e0f80a
EQuimper <quimperemanuel@gmail.com>
2016-12-11 19:34:56 -0500

    Make ajax request to backend

---
+++ public/javascript/main.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+$('#btn-short').on('click', function() {
+  // AJAX call to /api/shorten with the URL that the user entered in the input box
+  $.ajax({
+    url: '/api/v1/shorten',
+    type: 'POST',
+    dataType: 'JSON',
+    data: { longUrl: $('#url-to-short').val() },
+    success: function(data) {
+
+        console.log(data);
+        // display the shortened URL to the user that is returned by the server
+        var resultHTML = '<a class="result" href="' + data.url.shortUrl + '">'
+            + 'http:localhost:3000/' + data.url.shortUrl + '</a>';
+        $('#link').html(resultHTML);
+        $('#link').hide().fadeIn('slow');
+    }
+  });
+});
