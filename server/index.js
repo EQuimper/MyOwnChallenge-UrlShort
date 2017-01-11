297cf0c9243f10c8884d8fc111621629534545e7
EQuimper <quimperemanuel@gmail.com>
2017-01-11 12:50:58 -0500

    Change css and resolve server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV !== 'production') {
   mongoConf = 'mongodb://localhost/url';
 } else {
   app.use(express.static('dist'));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
   mongoConf = process.env.MONGO_URL;
@@ -41,7 +41,6 @@ mongoose.connection
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
 
 app.use([urlRoutes]);
 
