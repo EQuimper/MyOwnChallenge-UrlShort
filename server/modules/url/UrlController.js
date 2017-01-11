bfffac1da14e4e7a3365b4a2fd4fdc173d0dbfe5
EQuimper <quimperemanuel@gmail.com>
2017-01-11 13:27:11 -0500

    Change update

--- server/modules/url/UrlController.js
+++ server/modules/url/UrlController.js
@@ -45,7 +45,7 @@ export const redirectLong = (req, res) => {
 
   console.log('HELLO', { shortUrl });
 
-  return Url.findOneAndUpdate({ shortUrl })
+  return Url.findOne({ shortUrl })
     .then(url => {
       // if we dont find a url we redirect back to home page
       if (!url) { return res.redirect('/').json({ success: false, message: 'This url not exist in the system' }); }
