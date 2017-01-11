fbd758bd5a08156e70f3faf708b18bef486ab367
EQuimper <quimperemanuel@gmail.com>
2017-01-11 11:18:19 -0500

    Correct typo on server

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV !== 'production') {
 } else {
   app.use(express.static('dist'));
   app.get('*', (req, res) => {
-    res.sendFile(path.joint(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
   mongoConf = process.env.MONGO_URL;
 }
