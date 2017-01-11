e3ab5f32c31cbca0aa376f82caaa77fbe026abba
EQuimper <quimperemanuel@gmail.com>
2017-01-11 13:15:09 -0500

    Change the publickpath/

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[chunkhash].js',
+    publicPath: '/'
   },
   module: {
     rules: [
