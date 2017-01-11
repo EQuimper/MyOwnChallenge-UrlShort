251eee01eaf20d09a3850b8b4dfc3a4d456dc6b5
EQuimper <quimperemanuel@gmail.com>
2017-01-11 11:34:46 -0500

    Correction typo

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(webpackMiddleware(webpack(webpackConfig)));
   mongoConf = 'mongodb://localhost/url';
 } else {
-  app.use(express.static(path.joint(__dirname, '../dist')));
+  app.use(express.static(path.join(__dirname, '../dist')));
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
