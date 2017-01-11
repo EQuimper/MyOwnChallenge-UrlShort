52a11bf9d9f0d0bedd7089b6e9322e48588b3c9d
EQuimper <quimperemanuel@gmail.com>
2017-01-11 11:22:38 -0500

    Change dist

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(webpackMiddleware(webpack(webpackConfig)));
   mongoConf = 'mongodb://localhost/url';
 } else {
-  app.use(express.static('dist'));
+  app.use(express.static(__dirname + '/dist'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
