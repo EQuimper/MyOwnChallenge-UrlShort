2cc033bcb99e9cb17cb54bcb447fb6c74fe59cce
EQuimper <quimperemanuel@gmail.com>
2017-01-11 12:16:41 -0500

    Minified last version

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify('production')
       }
     }),
     new webpack.optimize.CommonsChunkPlugin({
